2e7dd7ea0273cd11dd93a0d3ba9a41926621a57a
Yohann DC <YohannDCz@gmail.com>
2023-05-03 09:16:08 +0200

    Finitions

--- lib/about/about.dart
+++ lib/about/about.dart
@@ -17,113 +17,116 @@ class About extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            elevation: 0.0,
-            title: Padding(
-              padding: const EdgeInsets.only(left: 16.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Placeholder()),
-                  );
-                },
-                child: SvgPicture.asset(
-                  'assets/logos/logo.svg',
-                  height: 32,
-                  width: 128, // adjust the logo size as needed
-                  semanticsLabel: 'My App Logo', // optional - provide a description for accessibility
-                ),
+          elevation: 0.0,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Placeholder()),
+                );
+              },
+              child: SvgPicture.asset(
+                'assets/logos/logo.svg',
+                height: 32,
+                width: 128, // adjust the logo size as needed
+                semanticsLabel: 'My App Logo', // optional - provide a description for accessibility
               ),
             ),
-            centerTitle: false,
           ),
+          centerTitle: false,
+        ),
         endDrawer: Drawer(
             width: 255.0,
             backgroundColor: const Color(0xFF2C6269),
             child: Padding(
               padding: const EdgeInsets.only(left: 32.0),
-              child: ListView(children: [
-                ListTile(
-                  title: Padding(
-                    padding: const EdgeInsets.only(top: 32.0, bottom: 12.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const Home()),
-                        );
-                      },
-                      child: const Text(
-                        "home",
-                        style: TextStyle(
-                          fontFamily: "Livvic",
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w600,
-                          height: 1.56,
-                          color: Colors.white,
+              child: Stack(children: [
+                ListView(children: [
+                  ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 32.0, bottom: 12.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const Home()),
+                          );
+                        },
+                        child: const Text(
+                          "home",
+                          style: TextStyle(
+                            fontFamily: "Livvic",
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w600,
+                            height: 1.56,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                ListTile(
-                  title: Padding(
-                    padding: const EdgeInsets.only(bottom: 36.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const About()),
-                        );
-                      },
-                      child: const Text(
-                        "about",
-                        style: TextStyle(
-                          fontFamily: "Livvic",
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w600,
-                          height: 1.56,
-                          color: Colors.white,
+                  ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.only(bottom: 36.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const About()),
+                          );
+                        },
+                        child: const Text(
+                          "about",
+                          style: TextStyle(
+                            fontFamily: "Livvic",
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w600,
+                            height: 1.56,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                ListTile(
-                  title: Container(
-                    width: 159.0,
-                    height: 48.0,
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white,
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(999.0)),
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom: 4.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const Contact()),
-                            );
-                          },
-                          child: const Text(
-                            "contact us",
-                            style: TextStyle(
-                              fontFamily: "Livvic",
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.w600,
-                              height: 1.56,
-                              color: Colors.white,
+                  ListTile(
+                    title: Container(
+                      width: 159.0,
+                      height: 48.0,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(999.0)),
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 4.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const Contact()),
+                              );
+                            },
+                            child: const Text(
+                              "contact us",
+                              style: TextStyle(
+                                fontFamily: "Livvic",
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w600,
+                                height: 1.56,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
+                ]),
+                Positioned(bottom: 0.0, right: -100.0, child: SvgPicture.asset("assets/background/bg-pattern-about-1-mobile-nav-1.svg"))
               ]),
             )),
         body: ListView(children: const [
--- lib/home/home.dart
+++ lib/home/home.dart
@@ -9,6 +9,7 @@ import 'package:myteam_multiplepage_website/home/testimonial.dart';
 
 import 'package:myteam_multiplepage_website/shared/contactus.dart';
 import 'package:myteam_multiplepage_website/shared/footer.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -46,89 +47,91 @@ class _HomeState extends State<Home> {
           backgroundColor: const Color(0xFF2C6269),
           child: Padding(
             padding: const EdgeInsets.only(left: 32.0),
-            child: ListView(children: [
-              ListTile(
-                title: Padding(
-                  padding: const EdgeInsets.only(top: 32.0, bottom: 12.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Home()),
-                      );
-                    },
-                    child: const Text(
-                      "home",
-                      style: TextStyle(
-                        fontFamily: "Livvic",
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w600,
-                        height: 1.56,
-                        color: Colors.white,
+            child: Stack(children: [
+              ListView(children: [
+                ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.only(top: 32.0, bottom: 12.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Home()),
+                        );
+                      },
+                      child: const Text(
+                        "home",
+                        style: TextStyle(
+                          fontFamily: "Livvic",
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w600,
+                          height: 1.56,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              ListTile(
-                title: Padding(
-                  padding: const EdgeInsets.only(bottom: 36.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const About()),
-                      );
-                    },
-                    child: const Text(
-                      "about",
-                      style: TextStyle(
-                        fontFamily: "Livvic",
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w600,
-                        height: 1.56,
-                        color: Colors.white,
+                ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.only(bottom: 36.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const About()),
+                        );
+                      },
+                      child: const Text(
+                        "about",
+                        style: TextStyle(
+                          fontFamily: "Livvic",
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w600,
+                          height: 1.56,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              ListTile(
-                title: Container(
-                  width: 159.0,
-                  height: 48.0,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 2.0,
-                    ), 
-                    borderRadius: BorderRadius.circular(999.0)
-                  ),
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 4.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const Contact()),
-                          );
-                        },
-                        child: const Text(
-                          "contact us",
-                          style: TextStyle(
-                            fontFamily: "Livvic",
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.w600,
-                            height: 1.56,
-                            color: Colors.white,
+                ListTile(
+                  title: Container(
+                    width: 159.0,
+                    height: 48.0,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2.0,
+                        ),
+                        borderRadius: BorderRadius.circular(999.0)),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 4.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const Contact()),
+                            );
+                          },
+                          child: const Text(
+                            "contact us",
+                            style: TextStyle(
+                              fontFamily: "Livvic",
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w600,
+                              height: 1.56,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
+              ]),
+              Positioned(bottom: 0.0, right: -100.0, child: SvgPicture.asset("assets/background/bg-pattern-about-1-mobile-nav-1.svg"))
             ]),
           )),
       body: ListView(
